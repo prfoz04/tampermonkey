6a62d53a9cce620027f8880d51fe01d599155ddf
prfoz04 <prfoz04@gmail.com>
2026-07-28 16:29:43 -0300

    tirando o embaçado do fundo

--- eproc/geracao de relatorios/src/geracao-de-relatorios.user.js
+++ eproc/geracao de relatorios/src/geracao-de-relatorios.user.js
@@ -320,7 +320,7 @@
             position: 'fixed', top: '0', left: '0', width: '100vw', height: '100vh',
             backgroundColor: 'rgba(0, 0, 0, 0.6)', display: 'flex',
             justifyContent: 'center', alignItems: 'center', zIndex: '99999',
-            fontFamily: 'Arial, sans-serif', backdropFilter: 'blur(2px)'
+            fontFamily: 'Arial, sans-serif',
         });
 
         // Caixa modal branca

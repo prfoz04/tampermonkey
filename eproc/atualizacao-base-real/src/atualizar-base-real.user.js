39c5e100bc668c34161fe3867394a94585def72c
prfoz04 <prfoz04@gmail.com>
2026-07-31 16:14:17 -0300

    correção da url RAW

--- eproc/atualizacao-base-real/src/atualizar-base-real.user.js
+++ eproc/atualizacao-base-real/src/atualizar-base-real.user.js
@@ -5,8 +5,8 @@
 // @description  Recolhe as informações de execução de pena do eproc e os insere nas devidas planilhas de entidade, a fim de normalizar os dados para vizualização no site
 // @author       Leonardo
 // @match        https://eproc.jfpr.jus.br/eprocV2/controlador.php?acao=pena_alternativa_consulta_interna*
-// @updateURL    https://raw.githubusercontent.com/prfoz04/tampermonkey/main/eproc/atualizacao-base-real/atualizar-base-real.user.js
-// @downloadURL  https://raw.githubusercontent.com/prfoz04/tampermonkey/main/eproc/atualizacao-base-real/atualizar-base-real.user.js
+// @updateURL    https://raw.githubusercontent.com/prfoz04/tampermonkey/main/eproc/atualizacao-base-real/src/atualizar-base-real.user.js
+// @downloadURL  https://raw.githubusercontent.com/prfoz04/tampermonkey/main/eproc/atualizacao-base-real/src/atualizar-base-real.user.js
 // @run-at       document-idle
 // ==/UserScript==
 

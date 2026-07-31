06f92e088a2da7fba521e46fa2bef20449b4a9b3
prfoz04 <prfoz04@gmail.com>
2026-07-31 15:34:45 -0300

    criação do ambiente

---
+++ eproc/atualizacao-base-real/src/atualizar-base-real.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         eproc - Atualizar banco de dados para o site (planilhas de entidade)
+// @namespace    https://github.com/4Vara
+// @version      1.0.0
+// @description  Recolhe as informações de execução de pena do eproc e os insere nas devidas planilhas de entidade, a fim de normalizar os dados para vizualização no site
+// @author       Leonardo
+// @match        https://eproc.jfpr.jus.br/eprocV2/controlador.php?acao=pena_alternativa_consulta_interna*
+// @updateURL    https://raw.githubusercontent.com/prfoz04/tampermonkey/main/eproc/atualizacao-base-real/atualizar-base-real.user.js
+// @downloadURL  https://raw.githubusercontent.com/prfoz04/tampermonkey/main/eproc/atualizacao-base-real/atualizar-base-real.user.js
+// @run-at       document-idle
+// ==/UserScript==
+
+(async function () {
+    'use strict';
+    console.log('iniciando script...');
+})();
